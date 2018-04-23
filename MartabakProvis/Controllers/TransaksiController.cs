dc84f73393e8065465eb3c97a462dc1fbbd7a148
ghfar1212 <36702581+ghfar1212@users.noreply.github.com>
2018-04-23 22:10:54 +0700

    nambah transaksi

---
+++ MartabakProvis/Controllers/TransaksiController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MartabakProvis.Repositories;
+using MartabakProvis.Models;
+
+namespace MartabakProvis.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Transaksi")]
+    public class TransaksiController : Controller
+    {
+        TransaksiRepo repo = new TransaksiRepo();
+        // GET: api/Transaksi
+        [HttpGet(Name = "GetAllTransaksi")]
+        public IEnumerable<TransaksiModel> GetAll()
+        {
+            var data = repo.GetAll();
+            return data;
+        }
+
+        // GET: api/Transaksi/5
+        [HttpGet("{id}", Name = "GetById")]
+        public IEnumerable<TransaksiModel> GetById(int id)
+        {
+            var data = repo.GetById(id);
+            yield return data;
+        }
+
+        // GET: api/Transaksi/pembeli/1
+        [HttpGet("pembeli/{id}", Name = "GetByIdPembeli")]
+        public IEnumerable<TransaksiModel> GetByIdPembeli(int id)
+        {
+            var data = repo.GetByIdPembeli(id);
+            return data;
+        }
+
+        // GET: api/Transaksi/tanggal/1
+        [HttpGet("tanggal/{tanggal}", Name = "GetByTanggal")]
+        public IEnumerable<TransaksiModel> GetByTanggal(string tanggal)
+        {
+            var data = repo.GetByTanggal(tanggal);
+            return data;
+        }
+
+        // POST: api/Transaksi
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/Transaksi/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MartabakProvis/Repositories/TransaksiRepo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MartabakProvis.Interface;
+using MartabakProvis.Models;
+using MartabakProvis.Helper;
+using Dapper.Contrib.Extensions;
+using Dapper;
+
+namespace MartabakProvis.Repositories
+{
+    public class TransaksiRepo : InterRepo<TransaksiModel>
+    {
+        Database db;
+        public TransaksiRepo()
+        {
+            db = new Database();
+        }
+
+        public List<TransaksiModel> GetAll()
+        {
+            db.Open();
+            var data = db.connection.GetAll<TransaksiModel>().ToList();
+            db.Close();
+            return data;
+        }
+
+        public List<TransaksiModel> GetByIdPembeli(int id)
+        {
+            db.Open();
+            var sql = "SELECT * FROM t_transaksi Where xid_pembeli = " + id;
+            TransaksiModel trs = new TransaksiModel();
+            var data = db.connection.Query<TransaksiModel>(sql, 
+                new {
+                    trs.id_transaksi,
+                    trs.tanggal,
+                    trs.jumlah_beli,
+                    trs.total_harga,
+                    trs.status,
+                    trs.xid_pembeli
+                }).ToList();
+            db.Close();
+            return data;
+        }
+
+        public List<TransaksiModel> GetByTanggal(string tanggal)
+        {
+            db.Open();
+            var sql = "SELECT * FROM t_transaksi Where tanggal = '" + tanggal+"'";
+            TransaksiModel trs = new TransaksiModel();
+            var data = db.connection.Query<TransaksiModel>(sql,
+                new
+                {
+                    trs.id_transaksi,
+                    trs.tanggal,
+                    trs.jumlah_beli,
+                    trs.total_harga,
+                    trs.status,
+                    trs.xid_pembeli
+                }).ToList();
+            db.Close();
+            return data;
+        }
+
+        public TransaksiModel GetById(int id)
+        {
+            db.Open();
+            var data = db.connection.Get<TransaksiModel>(id);
+            db.Close();
+            return data;
+        }
+
+        public bool Insert(TransaksiModel transaksi)
+        {
+            db.Open();
+            var data = db.connection.Insert<TransaksiModel>(transaksi);
+            db.Close();
+            return true;
+        }
+
+        public bool Update(TransaksiModel transaksi)
+        {
+            db.Open();
+            var data = db.connection.Update<TransaksiModel>(transaksi);
+            db.Close();
+            return true;
+        }
+
+        public bool Delete(TransaksiModel transaksi)
+        {
+            db.Open();
+            var data = db.connection.Delete<TransaksiModel>(transaksi);
+            db.Close();
+            return true;
+        }
+    }
+}

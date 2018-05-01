73a956b6bd8f805ea9fe5e3798c6f5e380a269ca
Afina Hadaina Yudianita <ydnta@yahoo.com>
2018-05-01 20:34:02 +0700

    controller size

--- MartabakProvis/Models/SizeModel.cs
+++ MartabakProvis/Models/SizeModel.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
 
 namespace MartabakProvis.Models
 {
+    [Table("t_size")]
     public class SizeModel
     {
+
+        [Key]
+        public int id_size { get; set; }
+        public int id_menu { get; set; }
+        public string size { get; set; }
+        public decimal harga { get; set; }
+
+        
     }
 }
---
+++ MartabakProvis/Repositories/SizeRepo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MartabakProvis.Helper;
+using MartabakProvis.Models;
+using MartabakProvis.Interface;
+using Dapper.Contrib.Extensions;
+using Dapper;
+
+namespace MartabakProvis.Repositories
+{
+    public class SizeRepo : InterRepo<SizeModel>
+    {
+        Database db;
+
+        public SizeRepo()
+        {
+            db = new Database();
+        }
+
+        public bool Insert(SizeModel size)
+        {
+            db.Open();
+            var data = db.connection.Insert<SizeModel>(size);
+            db.Close();
+            return true;
+        }
+        public bool Update(SizeModel size)
+        {
+            db.Open();
+            var data = db.connection.Update<SizeModel>(size);
+            db.Close();
+            return true;
+
+        }
+        public bool Delete(SizeModel size)
+        {
+            db.Open();
+            var data = db.connection.Delete<SizeModel>(size);
+            db.Close();
+            return true;
+
+        }
+        public SizeModel GetById(int id)
+        {
+            db.Open();
+            var data = db.connection.Get<SizeModel>(id);
+            db.Close();
+            return data;
+
+        }
+        public List<SizeModel> GetAll()
+        {
+            db.Open();
+            var data = db.connection.GetAll<SizeModel>().ToList();
+
+            db.Close();
+            return data;
+
+        }
+    }
+}

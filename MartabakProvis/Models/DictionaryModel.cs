b52cb4150fd44140f9ee16f2666e0fd280b0a9f8
Afina Hadaina Yudianita <ydnta@yahoo.com>
2018-05-16 14:13:57 +0700

    get dictionary by table + get all table name

---
+++ MartabakProvis/Models/DictionaryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MartabakProvis.Models
+{
+    public class DictionaryModel
+    {
+        public string column_name { get; set; }
+        public string data_type { get; set; }
+        public string is_nullable { get; set; }
+    }
+}
